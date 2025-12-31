ea868b790ce9200b7fae6eb4acb15316eb7022ef
Charles <ultramixerman@gmail.com>
2025-12-30 19:45:16 -0700

    Update logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,7 @@
-import { Levels, Logger, NDJsonTransport } from '@rabbit-company/logger';
+import { Levels, Logger } from "@rabbit-company/logger";
 
-const { LEVEL = 'INFO' } = process.env;
+const { LEVEL = "INFO" } = process.env;
 
 export const logger = new Logger({
   level: Levels[LEVEL as keyof typeof Levels],
-  transports: [new NDJsonTransport()]
 });
