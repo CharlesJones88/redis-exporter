a1d5392494b6825e79495432db9ef0783ad39abe
Charles <ultramixerman@gmail.com>
2026-01-11 16:43:43 -0700

    Update redis getClient logic

--- src/redis.ts
+++ src/redis.ts
@@ -4,9 +4,8 @@ const { REDIS_URL = "" } = Bun.env;
 let redis: RedisClient | undefined;
 
 export async function getClient() {
-  if (redis == undefined || !redis.connected) {
+  if (redis == undefined) {
     redis = new RedisClient(REDIS_URL);
-    await redis.connect();
   }
   return redis;
 }
